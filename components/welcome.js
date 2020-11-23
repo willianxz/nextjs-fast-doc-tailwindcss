6d5156cec099f5ef786e28b4dfd4369395e89695
Willian Duarte <willian@dzigual.com.br>
2020-11-23 16:33:23 -0300

    Estilizando Welcome

--- components/welcome.js
+++ components/welcome.js
@@ -3,7 +3,7 @@ export default function Welcome(){
 		<div>
 			<div className="grid justify-items-stretch">			  
 			  <div className="justify-self-center">
-			     <h1 className="font-mono text-8xl text-center">Welcome To Fast Doc Tailwindcss</h1>
+			     <h1 className="font-mono text-8xl text-center text-white bg-gradient-to-r from-black to-blue-500">Welcome To Fast Doc Tailwindcss</h1>
 			     <h3 className="font-mono text-2xl text-gray-600 text-center">
 			       A quick approach to using tailwindcss
 			     </h3>
