a3550e36b2c49b583beed9df55204063b4f2f21e
Willian Duarte <willian@dzigual.com.br>
2020-11-23 15:14:13 -0300

    Atualizando o infoText

--- components/textInfo.js
+++ components/textInfo.js
@@ -2,65 +2,121 @@ export default function TextInfo(){
 	return(
 		<div className="container mx-auto divide-y-4 divide-black divide-opacity-25">
 
-          <div>
-            <h3 className="font-mono text-3xl text-center capitalize hover:text-purple-600">text family</h3>     
-            <p className="font-sans text-3xl text-center text-gray-600">  
+          <div className="text-center">
+            <h3 className="font-mono text-3xl capitalize hover:text-purple-600">text family</h3>
+
+            <br/>
+
+            <p className="font-sans text-3xl text-gray-600">  
               font-sans
             </p>
-            <p className="font-serif text-3xl text-center text-gray-600">
+            <code>&lt;p className="font-sans text-3xl text-gray-600"&gt;font-sans&lt;/p&gt;</code>
+
+            <br/>
+            <br/>
+
+            <p className="font-serif text-3xl text-gray-600">
               font-serif
             </p>
-            <p className="font-mono text-3xl text-center text-gray-600">
+            <code>&lt;p className="font-serif text-3xl text-gray-600"&gt;font-serif&lt;/p&gt;</code>
+
+            <br/>
+            <br/>
+
+            <p className="font-mono text-3xl text-gray-600">
               font-mono
             </p>
+            <code>&lt;p className="font-mono text-3xl text-gray-600"&gt;font-mono&lt;/p&gt;</code>
+            <br/>
+            <br/>
 
-            <br/>
-            <br/>
           </div>
 
-         <div>
-            <h3 className="font-mono text-3xl text-center capitalize hover:text-purple-600">text color</h3>     
-            <p className="font-serif text-3xl text-center text-purple-300">  
+         <div className="text-center">
+            <h3 className="font-mono text-3xl capitalize hover:text-purple-600">text color</h3> 
+
+            <br/>    
+
+            <p className="font-serif text-3xl text-purple-300">  
               text-purple-300
             </p>
-            <p className="font-serif text-3xl text-center text-purple-600">
+            <code>&lt;p className="font-serif text-3xl text-purple-300"&gt;text-purple-300&lt;/p&gt;</code>
+
+            <br/>
+            <br/>
+
+            <p className="font-serif text-3xl text-purple-600">
               text-purple-600
             </p>
-            <p className="font-serif text-3xl text-center text-purple-900">
+            <code>&lt;p className="font-serif text-3xl text-purple-600"&gt;text-purple-600&lt;/p&gt;</code>
+
+            <br/>
+            <br/>
+
+            <p className="font-serif text-3xl text-purple-900">
               text-purple-900
             </p>
+            <code>&lt;p className="font-serif text-3xl text-purple-900"&gt;text-purple-900&lt;/p&gt;</code>
 
             <br/>
             <br/>
           </div>
 
-          <div>
-            <h3 className="font-mono text-3xl text-center capitalize hover:text-indigo-600">text background</h3>     
-            <p className="font-serif text-3xl text-center bg-indigo-300">
+          <div className="text-center">
+            <h3 className="font-mono text-3xl capitalize hover:text-indigo-600">text background</h3>  
+
+            <br/>    
+
+            <p className="font-serif text-3xl bg-indigo-300">
               bg-indigo-300
             </p>
-            <p className="font-serif text-3xl text-center bg-indigo-600">
+            <code>&lt;p className="font-serif text-3xl bg-indigo-300"&gt;bg-indigo-300&lt;/p&gt;</code>
+
+            <br/>
+            <br/>
+
+            <p className="font-serif text-3xl bg-indigo-600">
               bg-indigo-600
             </p>
-            <p className="font-serif text-3xl text-center bg-indigo-900">
+            <code>&lt;p className="font-serif text-3xl bg-indigo-600"&gt;bg-indigo-600&lt;/p&gt;</code>
+
+            <br/>
+            <br/>
+
+            <p className="font-serif text-3xl bg-indigo-900">
               bg-indigo-900
             </p>
+            <code>&lt;p className="font-serif text-3xl bg-indigo-900"&gt;bg-indigo-900&lt;/p&gt;</code>
 
             <br/>
             <br/>
           </div>
 
-          <div>
-            <h3 className="font-mono text-3xl text-center capitalize hover:text-green-600">text size</h3>     
-            <p className="font-serif text-center text-green-600 text-base">
+          <div className="text-center">
+            <h3 className="font-mono text-3xl capitalize hover:text-green-600">text size</h3> 
+
+            <br/>      
+            
+            <p className="font-serif text-base text-green-600">
               text-base
             </p>
-            <p className="font-serif text-center text-green-600 text-lg">
+            <code>&lt;p className="font-serif text-base text-green-600"&gt;text-base&lt;/p&gt;</code>
+
+            <br/>
+            <br/>
+
+            <p className="font-serif text-lg text-green-600">
               text-lg
             </p>
-            <p className="font-serif text-center text-green-600 text-5xl">
+            <code>&lt;p className="font-serif text-lg text-green-600"&gt;text-lg&lt;/p&gt;</code>
+
+            <br/>
+            <br/>
+
+            <p className="font-serif text-5xl text-green-600">
               text-5xl
             </p>
+            <code>&lt;p className="font-serif text-5xl text-green-600"&gt;text-5xl&lt;/p&gt;</code>
 
             <br/>
             <br/>
@@ -68,84 +124,154 @@ export default function TextInfo(){
 
 
 
-          <div>
-           <h3 className="font-mono text-3xl text-center capitalize hover:text-blue-600">text opacity</h3>     
-            <p className="font-serif text-3xl text-center text-blue-700 text-opacity-30">
+          <div className="text-center">
+           <h3 className="font-mono text-3xl capitalize hover:text-blue-600">text opacity</h3>
+
+           <br/>  
+
+            <p className="font-serif text-3xl text-blue-700 text-opacity-30">
               text-opacity-30
             </p>
-            <p className="font-serif text-3xl text-center text-blue-700 text-opacity-50">
+            <code>&lt;p className="font-serif text-3xl text-blue-700 text-opacity-30"&gt;text-opacity-30&lt;/p&gt;</code>
+
+            <br/>
+            <br/>
+
+            <p className="font-serif text-3xl text-blue-700 text-opacity-50">
               text-opacity-50
             </p>
-            <p className="font-serif text-3xl text-center text-blue-700 text-opacity-100">
+            <code>&lt;p className="font-serif text-3xl text-blue-700 text-opacity-50"&gt;text-opacity-50&lt;/p&gt;</code>
+
+            <br/>
+            <br/>
+
+            <p className="font-serif text-3xl text-blue-700 text-opacity-100">
               text-opacity-100
             </p>
+            <code>&lt;p className="font-serif text-3xl text-blue-700 text-opacity-100"&gt;text-opacity-100&lt;/p&gt;</code>
 
             <br/>
             <br/>
           </div>
 
 
-          <div>
-            <h3 className="font-mono text-3xl text-center capitalize hover:text-yellow-600">text transform</h3>     
-            <p className="font-serif text-3xl text-center text-yellow-600 uppercase">
+          <div className="text-center">
+            <h3 className="font-mono text-3xl capitalize hover:text-yellow-600">text transform</h3> 
+
+            <br/>
+
+            <p className="font-serif text-3xl text-yellow-600 uppercase">
               uppercase
             </p>
-            <p className="font-serif text-3xl text-center text-yellow-600 lowercase">
+            <code>&lt;p className="font-serif text-3xl text-yellow-600 uppercase"&gt;uppercase&lt;/p&gt;</code>
+
+            <br/>
+            <br/>
+
+            <p className="font-serif text-3xl text-yellow-600 lowercase">
               lowercase
             </p>
-            <p className="font-serif text-3xl text-center text-yellow-900 capitalize">
+            <code>&lt;p className="font-serif text-3xl text-yellow-600 lowercase"&gt;lowercase&lt;/p&gt;</code>
+
+            <br/>
+            <br/>
+
+            <p className="font-serif text-3xl text-yellow-900 capitalize">
               capitalize
             </p>
+            <code>&lt;p className="font-serif text-3xl text-yellow-900 capitalize"&gt;capitalize&lt;/p&gt;</code>
 
             <br/>
             <br/>
           </div>
 
-          <div>
-            <h3 className="font-mono text-3xl text-center capitalize hover:text-red-600">text effect hover color</h3>     
-            <p className="font-serif text-3xl text-center hover:text-red-300">
+          <div className="text-center">
+            <h3 className="font-mono text-3xl capitalize hover:text-red-600">text effect hover color</h3>
+
+            <br/>
+
+            <p className="font-serif text-3xl hover:text-red-300">
               hover:text-red-300
             </p>
-            <p className="font-serif text-3xl text-center hover:text-red-600">
+            <code>&lt;p className="font-serif text-3xl hover:text-red-300"&gt;hover:text-red-300&lt;/p&gt;</code>
+
+            <br/>
+            <br/>
+
+            <p className="font-serif text-3xl hover:text-red-600">
               hover:text-red-600
             </p>
-            <p className="font-serif text-3xl text-center hover:text-red-900">
+            <code>&lt;p className="font-serif text-3xl hover:text-red-600"&gt;hover:text-red-600&lt;/p&gt;</code>
+
+            <br/>
+            <br/>
+
+            <p className="font-serif text-3xl hover:text-red-900">
               hover:text-red-900
             </p>
+            <code>&lt;p className="font-serif text-3xl hover:text-red-900"&gt;hover:text-red-900&lt;/p&gt;</code>
 
             <br/>
             <br/>
           </div>
 
 
-          <div>
-            <h3 className="font-mono text-3xl text-center uppercase text-pink-600 hover:bg-pink-300">Text Effect Hover Background</h3>     
-            <p className="font-serif text-3xl text-center text-white-600 hover:bg-pink-300">
+          <div className="text-center">
+            <h3 className="font-mono text-3xl uppercase text-pink-600 hover:text-white hover:bg-pink-300">Text Effect Hover Background</h3>     
+            
+            <br/>
+
+            <p className="font-serif text-3xl hover:bg-pink-300">
               hover:bg-pink-300
             </p>
-            <p className="font-serif text-3xl text-center text-white-600 hover:bg-pink-600">
+            <code>&lt;p className="font-serif text-3xl hover:bg-pink-300"&gt;hover:bg-pink-300&lt;/p&gt;</code>
+
+            <br/>
+            <br/>
+
+            <p className="font-serif text-3xl hover:bg-pink-600">
               hover:bg-pink-600
             </p>
-            <p className="font-serif text-3xl text-center text-white-600 hover:bg-pink-900">
+            <code>&lt;p className="font-serif text-3xl hover:bg-pink-600"&gt;hover:hover:bg-pink-600&lt;/p&gt;</code>
+
+            <br/>
+            <br/>
+
+            <p className="font-serif text-3xl hover:bg-pink-900">
               hover:bg-pink-900
             </p>
+            <code>&lt;p className="font-serif text-3xl hover:bg-pink-900"&gt;hover:bg-pink-900&lt;/p&gt;</code>
 
             <br/>
             <br/>
           </div>
 
 
-           <div>
-              <h3 className="font-mono text-3xl text-center uppercase hover:text-blue-700">Text Effect Hover Opacity</h3>     
-              <p className="font-serif text-3xl text-center text-blue-700 hover:text-opacity-30">
+           <div className="text-center">
+              <h3 className="font-mono text-3xl uppercase hover:text-blue-700">Text Effect Hover Opacity</h3> 
+
+              <br/>    
+              
+              <p className="font-serif text-3xl text-blue-700 hover:text-opacity-30">
                hover:text-opacity-30
               </p>
-              <p className="font-serif text-3xl text-center text-blue-700 hover:text-opacity-50">
+              <code>&lt;p className="font-serif text-3xl text-blue-700 hover:text-opacity-30"&gt;hover:text-opacity-30&lt;/p&gt;</code>
+
+              <br/>
+              <br/>
+
+              <p className="font-serif text-3xl text-blue-700 hover:text-opacity-50">
                 hover:text-opacity-50
               </p>
-              <p className="font-serif text-3xl text-center text-blue-700 hover:text-opacity-100">
+              <code>&lt;p className="font-serif text-3xl text-blue-700 hover:text-opacity-50"&gt;hover:text-opacity-50&lt;/p&gt;</code>
+
+              <br/>
+              <br/>
+
+              <p className="font-serif text-3xl text-blue-700 text-opacity-30 hover:text-opacity-100">
                 hover:text-opacity-100
               </p>
+              <code>&lt;p className="font-serif text-3xl text-blue-700 text-opacity-30 hover:text-opacity-100"&gt;hover:text-opacity-100&lt;/p&gt;</code>
 
               <br/>
               <br/>
