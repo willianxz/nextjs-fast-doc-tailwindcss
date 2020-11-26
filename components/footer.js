b2f1cb3c34d0fad01a3c11c63a5c1ab7d9457e06
Willian Duarte <willian@dzigual.com.br>
2020-11-26 12:03:45 -0300

    Colocando botão de doação...

--- components/footer.js
+++ components/footer.js
@@ -3,6 +3,18 @@ import styles from '../styles/Home.module.css';
 export default function Footer(){
 	return(		
 		<div>
+			<form action="https://www.paypal.com/donate"
+				method="post" target="_top">
+				<input type="hidden" name="hosted_button_id" value="H68VWYW9QA8D2" />
+				<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif"
+				  border="0" name="submit" 
+				  title="PayPal - The safer, easier way to pay online!" 
+				  alt="Donate with PayPal button" 
+				 />
+				<img alt="" border="0"
+				src="https://www.paypal.com/en_BR/i/scr/pixel.gif" 
+				width="1" height="1" />
+			</form>
 			<a
 	          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
 	          target="_blank"
