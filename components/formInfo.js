88c8af9a0c69906b9ad207bb780085cfbf89759b
Willian Duarte <willian@dzigual.com.br>
2020-12-04 15:10:38 -0300

    Atualizando o formulario.

--- components/formInfo.js
+++ components/formInfo.js
@@ -103,10 +103,124 @@ export default function FormInfo(){
                     placeholder="Click to fill this input"
                   />
                 </div>
+              </div>   
+
+              <br/>
+
+              <div className="grid grid-cols-1 place-items-center"> 
+                <code className="select-all">
+                    &lt;div className="grid grid-cols-1 px-9 place-items-center"&gt;          
+                      &lt;div className="p-2 w-full"&gt; 
+                          &lt;h2 className="font-mono text-center text-3xl text-gray-600"&gt;Form Title&lt;/h2&gt;
+                          &lt;br/&gt;
+                          &lt;br/&gt;
+
+                          &lt;br/&gt;
+                          &lt;h2 className="font-mono text-center text-3xl text-gray-600"&gt;Information Session&lt;/h2&gt;             
+                          &lt;br/&gt;
+
+                          &lt;div className="grid grid-cols-4 place-items-center"&gt; 
+                            &lt;div&gt;&lt;/div&gt;
+                            &lt;div className="px-2"&gt;
+                              &lt;p className="text-center"&gt;Your Name&lt;/p&gt;
+                              &lt;input className="bg-gradient-to-r from-indigo-600 to-blue-600 flex-1 appearance-none border border-transparent w-full py-2 px-4 text-white placeholder-gray-300 shadow-xl rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent" 
+                                type="text" 
+                                placeholder="Click to fill this input"
+                              /&gt;
+                            &lt;/div&gt;
+                            &lt;div className="px-2"&gt;
+                              &lt;p className="text-center"&gt;Last Name&lt;/p&gt;
+                              &lt;input className="bg-gradient-to-r from-indigo-600 to-blue-600 flex-1 appearance-none border border-transparent w-full py-2 px-4 text-white placeholder-gray-300 shadow-xl rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent" 
+                                type="text" 
+                                placeholder="Click to fill this input"
+                              /&gt;
+                            &lt;/div&gt;
+                            &lt;div&gt;&lt;/div&gt;
+                          &lt;/div&gt;
+
+                          &lt;br/&gt;
+                          &lt;h3 className="font-mono text-center text-3xl text-gray-600"&gt;Contact Session&lt;/h3&gt;
+                          &lt;br/&gt;
+
+                          &lt;div className="grid grid-cols-3 place-items-center"&gt; 
+                            &lt;div className="px-2"&gt;
+                              &lt;p className="text-center"&gt;Your E-mail&lt;/p&gt;
+                              &lt;input className="bg-gradient-to-r from-indigo-600 to-blue-600 flex-1 appearance-none border border-transparent w-full py-2 px-4 text-white placeholder-gray-300 shadow-xl rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent" 
+                                type="email" 
+                                placeholder="you@exemple.com"
+                              /&gt;
+                            &lt;/div&gt;
+                            &lt;div className="px-2"&gt;
+                              &lt;p className="text-center"&gt;Home Phone&lt;/p&gt;
+                              &lt;input className="bg-gradient-to-r from-indigo-600 to-blue-600 flex-1 appearance-none border border-transparent w-full py-2 px-4 text-white placeholder-gray-300 shadow-xl rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent" 
+                                type="text" 
+                                placeholder="(DD)33350963"
+                              /&gt;
+                            &lt;/div&gt;
+                            &lt;div className="px-2"&gt;
+                              &lt;p className="text-center"&gt;Cell Phone&lt;/p&gt;
+                              &lt;input className="bg-gradient-to-r from-indigo-600 to-blue-600 flex-1 appearance-none border border-transparent w-full py-2 px-4 text-white placeholder-gray-300 shadow-xl rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent" 
+                                type="text" 
+                                placeholder="(DD)999276267"
+                              /&gt;
+                            &lt;/div&gt;
+                          &lt;/div&gt;
+
+
+                          &lt;br/&gt;
+                          &lt;h3 className="font-mono text-center text-3xl text-gray-600"&gt;Other Session&lt;/h3&gt;
+                          &lt;br/&gt;
+
+                          &lt;div className="grid grid-cols-4 place-items-center"&gt; 
+                            &lt;div className="px-2"&gt;
+                              &lt;p className="text-center"&gt;Other Information&lt;/p&gt;
+                              &lt;input className="bg-gradient-to-r from-indigo-600 to-blue-600 flex-1 appearance-none border border-transparent w-full py-2 px-4 text-white placeholder-gray-300 shadow-xl rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent" 
+                                type="text" 
+                                placeholder="Click to fill this input"
+                              /&gt;
+                            &lt;/div&gt;
+                            &lt;div className="px-2"&gt;
+                              &lt;p className="text-center"&gt;Other Information&lt;/p&gt;
+                              &lt;input className="bg-gradient-to-r from-indigo-600 to-blue-600 flex-1 appearance-none border border-transparent w-full py-2 px-4 text-white placeholder-gray-300 shadow-xl rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent" 
+                                type="text" 
+                                placeholder="Click to fill this input"
+                              /&gt;
+                            &lt;/div&gt;
+                            &lt;div className="px-2"&gt;
+                              &lt;p className="text-center"&gt;Other Information&lt;/p&gt;
+                              &lt;input className="bg-gradient-to-r from-indigo-600 to-blue-600 flex-1 appearance-none border border-transparent w-full py-2 px-4 text-white placeholder-gray-300 shadow-xl rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent" 
+                                type="text" 
+                                placeholder="Click to fill this input"
+                              /&gt;
+                            &lt;/div&gt;
+                            &lt;div className="px-2"&gt;
+                              &lt;p className="text-center"&gt;Other Information&lt;/p&gt;
+                              &lt;input className="bg-gradient-to-r from-indigo-600 to-blue-600 flex-1 appearance-none border border-transparent w-full py-2 px-4 text-white placeholder-gray-300 shadow-xl rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent" 
+                                type="text" 
+                                placeholder="Click to fill this input"
+                              /&gt;
+                            &lt;/div&gt;
+                          &lt;/div&gt;
+
+
+                          &lt;div className="grid grid-cols-1 place-items-center"&gt; 
+                            &lt;div className="px-2 w-full"&gt;
+                              &lt;p className="text-center"&gt;Other Information&lt;/p&gt;
+                              &lt;input className="bg-gradient-to-r from-indigo-600 to-blue-600 flex-1 appearance-none border border-transparent w-full py-2 px-4 text-white placeholder-gray-300 shadow-xl rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent" 
+                                type="text" 
+                                placeholder="Click to fill this input"
+                              /&gt;
+                            &lt;/div&gt;
+                          &lt;/div&gt;   
+
+                          &lt;br/&gt;
+
+                       &lt;/div&gt; 
+                    &lt;/div&gt; 
+                </code>
               </div>
 
-                   
-           </div> 
-        </div> 
+         </div> 
+     </div> 
 	);
 }
