dc12c0d15b94c03ddebbadf2711e8f557cc41f82
Willian Duarte <willian@dzigual.com.br>
2020-12-14 16:23:31 -0300

    Implementado mais uma parte para o header.

--- components/headerInfo.js
+++ components/headerInfo.js
@@ -135,10 +135,68 @@ export default function HeaderInfo(){
 		       <br/>
 		       <br/>
 
+		       <ul className="flex justify-center flex-wrap p-8 space-x-4 space-y-2">
+			        <li className="p-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 bg-blue-600 hover:bg-black">		         
+			            <a href="#" className="text-white no-underline uppercase">Home</a>
+			        </li>
+			        <li className="p-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 bg-blue-600 hover:bg-black hover:text-gray-600">
+			            <a href="#" className="no-underline hover:text-white uppercase">About</a>
+			        </li>
+			        <li className="p-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 bg-blue-600 hover:bg-black hover:text-gray-600">
+			            <a href="#" className="no-underline hover:text-white uppercase">Product</a>
+			        </li>
+			        <li className="p-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 bg-blue-600 hover:bg-black hover:text-gray-600">
+			            <a href="#" className="no-underline hover:text-white uppercase">Suport</a>
+			        </li>
+			         <li className="p-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 bg-blue-600 hover:bg-black hover:text-gray-600">
+			            <a href="#" className="no-underline hover:text-white uppercase">Business</a>
+			        </li>
+			        <li className="p-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 bg-blue-600 hover:bg-black hover:text-gray-600">
+			            <a href="#" className="no-underline hover:text-white uppercase">Contact</a>
+			        </li>
+			        <ul className="flex justify-center flex-wrap space-x-4 ">
+			         	<li className="p-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 bg-blue-600 hover:bg-black hover:text-gray-600">
+			              <a href="#" className=" no-underline hover:text-white uppercase">Login</a>
+		               </li>
+			        </ul>	       
+		       </ul>
 
+		       <div className="grid grid-cols-1 gap-2 place-items-center">
+				  <div className="bg-white p-2">
+				   <code className="select-all">
+				     &lt;div className="container mx-auto px-4"&gt;
+						&lt;ul className="flex justify-center flex-wrap p-8 space-x-4 space-y-2"&gt;
+						        &lt;li className="p-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 bg-blue-600 hover:bg-black"&gt;		         
+						            &lt;a href="#" className="text-white no-underline uppercase"&gt;Home&lt;/a&gt;
+						        &lt;/li&gt;
+						        &lt;li className="p-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 bg-blue-600 hover:bg-black hover:text-gray-600"&gt;
+						            &lt;a href="#" className="no-underline hover:text-white uppercase"&gt;About&lt;/a&gt;
+						        &lt;/li&gt;
+						        &lt;li className="p-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 bg-blue-600 hover:bg-black hover:text-gray-600"&gt;
+						            &lt;a href="#" className="no-underline hover:text-white uppercase"&gt;Product&lt;/a&gt;
+						        &lt;/li&gt;
+						        &lt;li className="p-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 bg-blue-600 hover:bg-black hover:text-gray-600"&gt;
+						            &lt;a href="#" className="no-underline hover:text-white uppercase"&gt;Suport&lt;/a&gt;
+						        &lt;/li&gt;
+						         &lt;li className="p-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 bg-blue-600 hover:bg-black hover:text-gray-600"&gt;
+						            &lt;a href="#" className="no-underline hover:text-white uppercase"&gt;Business&lt;/a&gt;
+						        &lt;/li&gt;
+						        &lt;li className="p-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 bg-blue-600 hover:bg-black hover:text-gray-600"&gt;
+						            &lt;a href="#" className="no-underline hover:text-white uppercase"&gt;Contact&lt;/a&gt;
+						        &lt;/li&gt;
+						        &lt;ul className="flex justify-center flex-wrap space-x-4 "&gt;
+						         	&lt;li className="p-2 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 bg-blue-600 hover:bg-black hover:text-gray-600"&gt;
+						              &lt;a href="#" className=" no-underline hover:text-white uppercase"&gt;Login&lt;/a&gt;
+					               &lt;/li&gt;
+						       &lt;/ul&gt;	       
+					    &lt;/ul&gt;
+					  &lt;/div&gt;
+				   </code>
+				  </div>
+				</div>
 
-
-
+		       <br/>
+		       <br/>
 
 
 
